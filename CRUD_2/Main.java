d9a695ea27343c2cae0f646dc2789743774bc2ee
Rafael <raphao5555@gmail.com>
2026-07-15 00:58:26 +0000

    atualizacao crud 2

--- CRUD_2/Main.java
+++ CRUD_2/Main.java
@@ -12,35 +12,77 @@ public class Main {
         contatinhos.setNome("Rafael");
         contatinhos.setNumero("9002 8922");
         listaDeContatos.add(contatinhos);
+        boolean ativado = true;
 
+        while (ativado) {
+            System.out.println("1.lista de contatos\n2.Adicionar contato\n3.Alterar contato\n4.remover contato");
 
-        System.out.println("1.lista de contatos\n2.Adicionar contato");
+            int painel = 0;
+            painel = input.nextInt();
+            input.nextLine();
 
-        int painel = 0;
-        painel = input.nextInt();
-        input.nextLine();
-
-        if (painel == 1 ) {
-
-            for (Contato contato : listaDeContatos) {
-                System.out.println(contato.getNome());
-                System.out.println(contato.getNumero());
+            if (painel == 1 ) {
+                System.out.println("===============");
+                for (int i = 0; i < listaDeContatos.size(); i++) {
+                    Contato contato = listaDeContatos.get(i);
+                    System.out.println(i + ". " +  contato.getNome() + "\n" + contato.getNumero());
+                    
+                }
+                System.out.println("===============");
+                painel = 0;
             }
-        }
 
-        if (painel == 2) {
-            Contato c1 = new Contato();
-            c1.setNome(input.nextLine());
-            c1.setNumero(input.nextLine());
-            listaDeContatos.add(c1);
 
-            System.out.println("-----------Lista atualizada------------");
+            if (painel == 2) {
+                Contato c1 = new Contato();
+                c1.setNome(input.nextLine());
+                c1.setNumero(input.nextLine());
+                listaDeContatos.add(c1);
 
-             for (Contato contato : listaDeContatos) {
-                System.out.println(contato.getNome());
-                System.out.println(contato.getNumero());
+                System.out.println("-----------Lista atualizada------------");
+
+                for (Contato contato : listaDeContatos) {
+                    System.out.println(contato.getNome());
+                    System.out.println(contato.getNumero());
+                }
+                System.out.println("===============");
+
+                painel = 0;
+                
             }
-            
+
+            if (painel == 3) {
+                System.out.println("Digite o nome do contato que deseja alterar");
+                String nomeContato = input.nextLine();
+                System.out.println("Digite novo nome");
+                String novoContato = input.nextLine();
+                System.out.println("===============");
+
+                for (Contato contato : listaDeContatos) {
+
+                    if (contato.getNome().equals(nomeContato)) {
+                        contato.setNome(novoContato);
+                        
+                    }
+
+
+                }
+                System.out.println("===============");
+                painel = 0;
+                
+            }
+
+            if (painel == 4) {
+                System.out.println("Digite o nome que deseja remover");
+                String nomeRemover = input.nextLine();
+
+                for (Contato contato : listaDeContatos) {
+                    if (contato.getNome().equals(nomeRemover)) {
+                        listaDeContatos.remove(nomeRemover);
+                    }
+                }
+            }
+
         }
 
         input.close();
